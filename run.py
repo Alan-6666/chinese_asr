77e04d3e1b0d398080652b3c0179511ffb062218
Alan-6666 <53568883+Alan-6666@users.noreply.github.com>
2021-11-18 21:57:41 +0800

    Update run.py

--- run.py
+++ run.py
@@ -17,43 +17,34 @@ if __name__=="__main__":
     wav_files, labels = get_wavs_and_labels(data_path,test_path,file_name)   
     
 #2、获取字典
+    dictionary_path = "data_file/thchs_30/dictionary.txt"
     
-
+    with open(dictionary_path) as f:
+        dictionary = f.readlines()    
+    
 #3、计算标准化特征
-
+    stand_para_path = "data_file/thchs_30/stand_nor.txt"
 
 #4、训练
     dataset = "thchs_30"    
-    dictionary_path = './txt_data/{}/dictionary.txt'.format(dataset) 
-    save_address    = "./model_file/{}".format(dataset)
+    save_address    = "model_file/{}".format(dataset)
     model_name      = "bi_lstm_150_nl"
-    train_data_path = 'txt_data/{}/train.txt'.format(dataset)
-    test_data_path  = "txt_data/{}/test.txt".format(dataset)
-    stand_para_path = save_address + "/stand_nor.txt"
+    train_data_path = save_address + "/train.txt'
+    test_data_path  = save_address + "/test.txt'
     epochs    = 150
-    with open(dictionary_path) as f:
-        temp = f.readlines()
- 
+    #定义参数
     input_size, hidden_size, num_layers, out_size = 80, 512, 1, len(temp)
-#    model = RNN(input_size  = input_size ,
-#                hidden_size = hidden_size, 
-#                num_layers  = num_layers , 
-#                out_size    = out_size   ,        
-#   )
     model = bi_lstm(
                 input_size  = input_size ,
                 hidden_size = hidden_size, 
                 num_layers  = num_layers , 
                 out_size    = out_size   ,        
 )
-  
-#    print(model)
-#    torch.cuda.set_device(0)
     if torch.cuda.is_available():
          device = torch.device("cuda")
     model = torch.nn.DataParallel(model, device_ids=[0, 1, 2])
 ###################### 训练 #################################################
-    train(model, save_address, model_name, epochs, train_data_path, test_data_path,dictionary_path, stand_para_path,device) 
+    train(model, save_address, model_name, epochs, train_data_path, test_data_path,dictionary, stand_para_path,device) 
 
 ###################### 测试 #################################################   
 #    model = rnn()
